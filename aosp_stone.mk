0612e2988b7530da95e992ed6a42756e2a8f3ab2
Agatha senpai <naufalmarjan2014@gmail.com>
2023-11-09 17:19:34 +0000

    stone: Welcome to Pixel based rom bringup

--- aosp_stone.mk
+++ aosp_stone.mk
@@ -8,13 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-PRODUCT_NAME := lineage_stone
+# Rom Specific Flags
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
+USE_PIXEL_CHARGING := true
+TARGET_BUILD_APERTURE_CAMERA := false
+
+# Device identifier
+PRODUCT_NAME := aosp_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
